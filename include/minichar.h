51afe04aa92c34ab299dc0f40a1a989e907995a5
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-24 01:42:58 -0400

    update interface

---
+++ include/minichar.h
@@ -0,0 +1,8 @@
+/*
+* Creates a compressed c string from the input string
+*/
+char* encode(char* str);
+/*
+* decodes the compressed string back to read its actual character values
+*/
+char* decode(char* str);
