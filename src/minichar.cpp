606bc14f1333581426659b8b4a7266a297faebe0
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-08-17 11:08:57 -0400

    actually modify res before returning

--- src/minichar.cpp
+++ src/minichar.cpp
@@ -2,26 +2,27 @@
 #include <cstring>
 #include "../include/minichar.h"
 
-const int chunkWidth = 5;
+const int COMP_WIDTH = 5;
+const int CHUNK_WIDTH = 8;
 
 char* encode(char* str) {
   int numChunks = std::floor(strlen(str) / 8 + 1);
-  char* res = new char[numChunks * chunkWidth + 1];
-  res[numChunks * chunkWidth] = '\0';
+  char* res = new char[numChunks * CHUNK_WIDTH + 1];
+  res[numChunks * CHUNK_WIDTH] = '\0';
   for (int num = 0; num < numChunks; ++num) {
-    char c1 = guardRange(str, num * chunkWidth + 0);
-    char c2 = guardRange(str, num * chunkWidth + 1);
-    char c3 = guardRange(str, num * chunkWidth + 2);
-    char c4 = guardRange(str, num * chunkWidth + 3);
-    char c5 = guardRange(str, num * chunkWidth + 4);
-    char c6 = guardRange(str, num * chunkWidth + 5);
-    char c7 = guardRange(str, num * chunkWidth + 6);
-    char c8 = guardRange(str, num * chunkWidth + 7);
-    char f1 = (c1 << 3) | (c2 >> 2);
-    char f2 = (c2 << 6) | (c3 << 1) | (c4 >> 4);
-    char f3 = (c4 << 4) | (c5 >> 1);
-    char f4 = (c5 << 7) | (c6 << 2) | (c7 >> 3);
-    char f5 = (c7 << 5) | (c8);
+    char c1 = guardRange(str, num * CHUNK_WIDTH + 0);
+    char c2 = guardRange(str, num * CHUNK_WIDTH + 1);
+    char c3 = guardRange(str, num * CHUNK_WIDTH + 2);
+    char c4 = guardRange(str, num * CHUNK_WIDTH + 3);
+    char c5 = guardRange(str, num * CHUNK_WIDTH + 4);
+    char c6 = guardRange(str, num * CHUNK_WIDTH + 5);
+    char c7 = guardRange(str, num * CHUNK_WIDTH + 6);
+    char c8 = guardRange(str, num * CHUNK_WIDTH + 7);
+    res[num * COMP_WIDTH] = (c1 << 3) | (c2 >> 2);
+    res[num * COMP_WIDTH + 1] = (c2 << 6) | (c3 << 1) | (c4 >> 4);
+    res[num * COMP_WIDTH + 2] = (c4 << 4) | (c5 >> 1);
+    res[num * COMP_WIDTH + 3] = (c5 << 7) | (c6 << 2) | (c7 >> 3);
+    res[num * COMP_WIDTH + 4] = (c7 << 5) | (c8);
   }
   return res;
 }
