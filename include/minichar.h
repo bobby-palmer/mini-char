45ae34a4cac3a04cf55de1c9ec1e9d750f8cf9bc
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-08-17 11:04:38 -0400

    implemented encoding function

--- include/minichar.h
+++ include/minichar.h
@@ -2,7 +2,14 @@
 * Creates a compressed c string from the input string
 */
 char* encode(char* str);
+
 /*
 * decodes the compressed string back to read its actual character values
 */
 char* decode(char* str);
+
+/*
+* Returns the rangified character at the index if the index is legit,
+* otherwise returns a placeholder to fix the 5 bit buffer
+*/
+char guardRange(char* str, int i);
--- src/minichar.cpp
+++ src/minichar.cpp
@@ -1,18 +1,37 @@
 #include <cmath>
 #include <cstring>
-#include "../src/5bit.h"
+#include "../include/minichar.h"
+
+const int chunkWidth = 5;
 
 char* encode(char* str) {
-  int len = std::strlen(str);
-  int adjLen = len + (8 - len % 8);
-  // extra character for null byte
-  char* sized = new char[adjLen + 1];
-  for (int i = 0; i < len; ++i) {
-    sized[i] = str[i];
+  int numChunks = std::floor(strlen(str) / 8 + 1);
+  char* res = new char[numChunks * chunkWidth + 1];
+  res[numChunks * chunkWidth] = '\0';
+  for (int num = 0; num < numChunks; ++num) {
+    char c1 = guardRange(str, num * chunkWidth + 0);
+    char c2 = guardRange(str, num * chunkWidth + 1);
+    char c3 = guardRange(str, num * chunkWidth + 2);
+    char c4 = guardRange(str, num * chunkWidth + 3);
+    char c5 = guardRange(str, num * chunkWidth + 4);
+    char c6 = guardRange(str, num * chunkWidth + 5);
+    char c7 = guardRange(str, num * chunkWidth + 6);
+    char c8 = guardRange(str, num * chunkWidth + 7);
+    char f1 = (c1 << 3) | (c2 >> 2);
+    char f2 = (c2 << 6) | (c3 << 1) | (c4 >> 4);
+    char f3 = (c4 << 4) | (c5 >> 1);
+    char f4 = (c5 << 7) | (c6 << 2) | (c7 >> 3);
+    char f5 = (c7 << 5) | (c8);
   }
-  for (int i = len; i < adjLen; i++) {
-    sized[i] = '%';
-  }
-  sized[adjLen] = '\0';
-  int newLen = std::ceil(adjLen / 8) * 5;
+  return res;
+}
+
+char* decode(char* str) {
+  // for compilation
+  return 0x00;
+}
+
+char guardRange(char *str, int i) {
+  //for compilation
+  return 'a';
 }
